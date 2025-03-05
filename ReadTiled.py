8d949d4b7c4e79993ac738d9f0775411845a9541
Jan Ilavsky <jilavsky@gmail.com>
2025-03-04 20:33:55 -0700

    Get Sxan type, file name and path to data from Tiled.

--- ReadTiled.py
+++ ReadTiled.py
@@ -1,48 +1,86 @@
-from tiled.client import from_uri
-from tiled.utils import tree
-from tiled.queries import FullText
+# readTiled.py 
+# version 0.1   2025-03-04
 
 
-client = from_uri("http://0.0.0.0:8020")
+# this code will get last N data sets from tiled database
+# it will recreate what IR3BS_GetJSONScanData() does in Indra package In3_ClueSkyreader.ipf
+# it wil luse tiled web interface and read data into json.
+# the main purpose is to get list fo filenames and paths for the last N scans
+# and then use this list to download the data from the server
+#the code will aslo remeber when was the last polling and ask for data only since last time. 
+# need to handel gracefully the start
+# code will trigger appropriate data recduction routines for different data sets
+# at the end the code will generate necessary pictures for live data page.
+# the code will be run in background and be checked by a cron job every 5 minutes
 
-# List available catalogs
-catalog_names = list(client)
-#print("Available catalogs:", catalog_names)
-
-# Replace 'your_catalog_name' with the actual name of the catalog you want to access
-catalog = client[catalog_names[0]]
-
-# List all keys in the catalog
-all_keys = list(catalog)
-#print("Catalog keys:", all_keys)
-
-# Get the last 10 records
-last_10_records = all_keys[-10:]
-#print("Last 10 records:", last_10_records)
-# Retrieve the last 10 entries using the keys
-last_10_entries = [catalog[key] for key in last_10_records]
-# Extract titles
-#c =  catalog[last_10_records]
-for c   in last_10_entries:
-    #print(c.metadata['title'])
-    titleCont=c.search(FullText("title`"))
-    print(titleCont[0])
+# method used buitls on https://github.com/BCDA-APS/bdp-tiled/blob/main/demo_client.ipynb
+# and follows Igor code to get the right data sets
 
 
-#LIstOftitles = [entry.metadata['title'] for entry in last_10_entries]
-#print("Filenames of the last 1 records:", LIstOftitles)
+# import necessary libraries
+import requests
+import json
+import datetime
 
-# List all keys in the catalog
-#all_keys = list(catalog)
 
-# Get the last 10 keys
-#last_10_keys = all_keys[-10:]
+def iso_to_ts(isotime):
+    return datetime.datetime.fromisoformat(isotime).timestamp()
 
-# Retrieve the last 10 entries using the keys
-#last_10_entries = [catalog[key] for key in last_10_keys]
+server = "localhost"
+port = 8020
+catalog = "usaxs"
 
-#print(last_10_entries)
+# Find all runs in a catalog between these two ISO8601 dates.
+start_time = "2025-02-15"
+end_time = "2025-02-25"
+tz = "US/Central"
 
-# Extract UUIDs (assuming each entry has a 'metadata' attribute with a 'uuid' key)
-#u#uids = [entry.metadata['uuid'] for entry in last_10_entries]
-#print("UUIDs of the last 10 records:", uuids)
+
+#  http://10.211.55.7:8020/api/v1/search//usaxs/?page[offset]=0
+# &page[limit]=100
+# &filter[time_range][condition][since]=1738738800
+# &filter[time_range][condition][timezone]=US/Central
+# &sort=time
+# &fields=metadata
+# &omit_links=true
+# &select_metadata={detectors:start.detectors,motors:start.motors,plan_name:start.plan_name,time:start.time,
+# scan_title:start.plan_args.scan_title,title:start.title,hdf5_file:start.hdf5_file,hdf5_path:start.hdf5_path}
+uri =(
+    f"http://{server}:{port}"  # standard prefix
+    "/api/v1/search"    # API command
+    f"/{catalog}"       # catalog
+    "?"                 # begin any command options
+    "page[limit]=10"   # 0: all matching
+    "&"                 # separator between any additional options
+    f"filter[time_range][condition][since]={iso_to_ts(start_time)}"
+    f"&filter[time_range][condition][until]={iso_to_ts(end_time)}"
+    f"&filter[time_range][condition][timezone]={tz}"
+    "&sort=time"
+    "&fields=metadata"
+    "&omit_links=true"
+    "&select_metadata={plan_name:start.plan_name,time:start.time,scan_title:start.plan_args.scan_title,\
+                        hdf5_file:start.hdf5_file,hdf5_path:start.hdf5_path}"
+)
+#print(f"{uri=}")
+r = requests.get(uri).json()
+#print(r["data"][0])
+
+def print_results_summary(r):
+    """We'll use this a few times."""
+    xref = dict(First=0, Last=-1)
+    for k, v in dict(First=0, Last=-1).items():
+        md = r["data"][v]["attributes"]["metadata"]["selected"]  #this is for UBUNTU VM, usaxscontrol does not have selected
+        #print(md)
+        # md keys: start  stop  summary
+        # summary key is composed by tiled server
+        plan_name = md["plan_name"]
+        #scan_id = md["data"]["id"]
+        #started = md["data"]["datetime"]
+        hdf5_file = md["hdf5_file"]
+        hdf5_path = md["hdf5_path"]
+        print(f"{k:5s} run: {plan_name=} path: {hdf5_path=} {hdf5_file=}")
+        
+print(f'Search of {catalog=} has {len(r["data"])} runs.')
+print_results_summary(r)
+
+
