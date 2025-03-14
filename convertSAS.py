14a8c78f56ffe66a857d15713fb585ac5e5029d3
Jan Ilavsky <jilavsky@gmail.com>
2025-03-13 20:11:22 -0600

    Trying to figure out pyFAI tilts. No luck yet.

--- convertSAS.py
+++ convertSAS.py
@@ -1,7 +1,6 @@
 # this file will convert SAXS and WAXS area detector data from the HDF5 format to the 1Ddata
 # we will use pyFai to do the conversion
 
-import pyFAI
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
@@ -10,6 +9,7 @@ import h5py
 from supportFunctions import read_group_to_dict
 import pprint as pp
 import os
+import tifffile as tiff
 
 # Load your 2D image data
 # For this example, let's assume you have a NumPy array `image_data` representing your detector image
@@ -40,25 +40,51 @@ def ImportAndReduceAD(path, filename):
         # poni1, point of intercept. 
         if "pin_ccd_tilt_x" in metadata_dict:
             usingWAXS=0
-            poni1 = instrument_dict["detector"]["beam_center_x"] # based on Peter's code this shoudl be opposite
-            poni2 = instrument_dict["detector"]["beam_center_y"] # poni2 shoudl be x and poni1 should be y
-            poni1 = poni1 * pixel_size1 
-            poni2 = poni2 * pixel_size2 
-            rot1 = metadata_dict["pin_ccd_tilt_x"]*np.pi/180
-            rot2 = metadata_dict["pin_ccd_tilt_y"]*np.pi/180
+            BCY = instrument_dict["detector"]["beam_center_y"] # based on Peter's code this shoudl be opposite
+            BCX = instrument_dict["detector"]["beam_center_x"] # poni2 shoudl be x and poni1 should be y
+            BCY = BCY * pixel_size2 
+            BCX = BCX * pixel_size1 
+            rotX = metadata_dict["pin_ccd_tilt_x"]*np.pi/180
+            rotY = metadata_dict["pin_ccd_tilt_y"]*np.pi/180
         else:
             usingWAXS=1
-            poni1 = instrument_dict["detector"]["beam_center_x"] # based on Peter's code this shoudl be opposite
-            poni2 = instrument_dict["detector"]["beam_center_y"] # poni2 shoudl be x and poni1 should be y
-            poni1 = poni1 * pixel_size1 
-            poni2 = poni2 * pixel_size2 
-            rot1 = metadata_dict["waxs_ccd_tilt_x"]*np.pi/180
-            rot2 = metadata_dict["waxs_ccd_tilt_y"]*np.pi/180     
-    #note the rot1 and rot2 make no sense - they are defined differently and anything
-    # different than 0 will cause the code to fail.
-    rot1=0.0
-    rot2=0.0
-            
+            BCY = instrument_dict["detector"]["beam_center_y"] # based on Peter's code this shoudl be opposite
+            BCX = instrument_dict["detector"]["beam_center_x"] # poni2 shoudl be x and poni1 should be y
+            BCY = BCY * pixel_size2 
+            BCX = BCX * pixel_size1 
+            rotX = metadata_dict["waxs_ccd_tilt_x"]*np.pi/180
+            rotY = metadata_dict["waxs_ccd_tilt_y"]*np.pi/180     
+
+    # now we need to do correction on geometry. 
+    # Nika rotates the detector around the beam center while pyFAI around sample
+    # this corrections testing is based on pyFAI-calib2 testing for 45 deg tilt tiff file from Bob GSAS-II
+    # first correct distacne:
+    #detector_distance = detector_distance*abs(np.cos(rotX)*np.cos(rotY))
+    # now calculate poni1 and poni2
+    # testing shows, that poni1 is related to BCY and rotY
+    # but rot1 is = rotX
+    # poni1 = BCX + detector_distance*np.tan(rotX)
+    # poni2 = BCY + detector_distance*np.tan(rotY)
+    # rot1=rotX
+    # rot2=rotY   
+    #override for for now below:
+    #TODO: need to figrue this out more on new test dat aset in hdf5 file with 
+    #proper calibration in parameters...        
+    rot1=0
+    rot2=0
+    poni1=BCX
+    poni2 = BCY
+    # print(f"wavelength: {wavelength}")
+    # print(f"pixel_size1: {pixel_size1}")
+    # print(f"pixel_size2: {pixel_size2}")
+    # print(f"detector_distance: {detector_distance}")
+    # print(f"poni1: {poni1}")
+    # print(f"poni2: {poni2}")
+    # print(f"rot1: {rot1}")
+    # print(f"rot2: {rot2}")    
+    # print(f"rotX: {rotX}")
+    # print(f"rotY: {rotY}")
+                
     # plt.imshow(my2DData, cmap='viridis',norm=LogNorm())  # You can choose different colormaps
     # plt.colorbar()  # Optional: Add a colorbar to show the scale
     # plt.title('2D Array Visualization')
@@ -85,14 +111,7 @@ def ImportAndReduceAD(path, filename):
     #detector_distance = 0.1  # in meters
     #pixel_size = 0.0001  # in meters
     #wavelength = 1.54e-10  # in meters (for example, Cu K-alpha)
-    # print(f"wavelength: {wavelength}")
-    # print(f"pixel_size1: {pixel_size1}")
-    # print(f"pixel_size2: {pixel_size2}")
-    # print(f"detector_distance: {detector_distance}")
-    # print(f"poni1: {poni1}")
-    # print(f"poni2: {poni2}")
-    # print(f"rot1: {rot1}")
-    # print(f"rot2: {rot2}")
+
     # Create an AzimuthalIntegrator object
     ai = AzimuthalIntegrator(dist=detector_distance, poni1=poni1, poni2=poni2, rot1=rot1, rot2=rot2,
                             pixel1=pixel_size1, pixel2=pixel_size2, 
@@ -107,6 +126,84 @@ def ImportAndReduceAD(path, filename):
     result = {"Intensity":np.ravel(intensity), "Q_array":np.ravel(q)}
     #pp.pprint(result)
     return result
+
+
+## test fro tilts goes here
+def test(path, filename):
+    # read data from tiff file and read the data 
+    my2DData = tiff.imread(filename)
+    # plt.imshow(my2DData, cmap='viridis',norm=LogNorm())  # You can choose different colormaps
+    # plt.colorbar()  # Optional: Add a colorbar to show the scale
+    # plt.title('2D Array Visualization')
+    # plt.show()    # wavelength, convert to m
+    wavelength = 0.10798* 1e-10 
+    # pixel_size, convert to m
+    pixel_size1 = 0.1* 1e-3 # x in Nika, in m
+    pixel_size2 = 0.1* 1e-3 # y in Nika, in m
+    # detector_distance, convert to m
+    detector_distance = 1004.91* 1e-3 
+    # poni1, point of intercept in y in Nika, in m
+    # poni2, point of intercept in x in Nika, in m
+    # read Nika BCX and BCY and convert to m
+    BCX = 886.7     # x in Nika
+    BCY = 1048.21   # y in Nika, width in Python
+    BCY = BCY * pixel_size2 #in m now
+    BCX = BCX * pixel_size1 #in m now
+    # read Nika rot1 and rot2 and convert to radians
+    rotX = 44.7*np.pi/180   # x direction in Nika
+    rotY = 0.02*np.pi/180   # y direction in Nika
+    # now corrections based on pyfain-geom2 testing for 45 deg tilt
+    # first correct distacne:
+    detector_distance = detector_distance*abs(np.cos(rotX)*np.cos(rotY))
+    # now calculate poni1 and poni2
+    # confusingly, the poni1 is realted to BCY and poni2 is related to BCX
+    # is this issue with reading tiff vs hdf5 image orientations? 
+    poni2 = BCX + detector_distance*np.tan(rotX)
+    poni1 = BCY + detector_distance*np.tan(rotY)
+    rot1=rotX
+    rot2=rotY              
+    #print(f"wavelength: {wavelength}")
+    #print(f"pixel_size1: {pixel_size1}")
+    #print(f"pixel_size2: {pixel_size2}")
+    # print(f"detector_distance: {detector_distance}")
+    # print(f"poni1: {poni1}")
+    # print(f"poni2: {poni2}")
+    # print(f"rot1: {rot1}")
+    # print(f"rot2: {rot2}")
+                
+    # plt.imshow(my2DData, cmap='viridis',norm=LogNorm())  # You can choose different colormaps
+    # plt.colorbar()  # Optional: Add a colorbar to show the scale
+    # plt.title('2D Array Visualization')
+    # plt.show()
+
+    #create mask here. Duplicate the my2DData and set all values above 1e7 to NaN
+    #this is for waxs ONLY, DIFFERENT FOR saxs
+    mask = np.copy(my2DData)
+    mask = 0*mask   # set all values to zero
+ 
+
+    # Define your detector geometry
+    # You need to specify parameters like the detector distance, pixel size, and wavelength
+    #detector_distance = 0.1  # in meters
+    #pixel_size = 0.0001  # in meters
+    #wavelength = 1.54e-10  # in meters (for example, Cu K-alpha)
+
+    # Create an AzimuthalIntegrator object
+    ai = AzimuthalIntegrator(dist=detector_distance, poni1=poni1, poni2=poni2, rot1=rot1, rot2=rot2,
+                            pixel1=pixel_size1, pixel2=pixel_size2, 
+                            wavelength=wavelength)
+
+    # Perform azimuthal integration
+    # You can specify the number of bins for the integration
+    #set npt to larger of dimmension of my2DData  `
+    npt = max(my2DData.shape)
+    #npt = 1000  # Number of bins
+    q, intensity = ai.integrate1d(my2DData, npt, mask=mask, correctSolidAngle=True, unit="q_A^-1")
+    result = {"Intensity":np.ravel(intensity), "Q_array":np.ravel(q)}
+    #pp.pprint(result)
+    return result
+
+
 
 # intensity=np.log(intensity)
 # # Plot the integrated intensity
@@ -140,13 +237,14 @@ def PlotResults(data_dict):
     plt.title('Plot of Intensity vs. Q')
     plt.xlabel('log(Q) [1/A]')
     plt.ylabel('Intensity')
-    plt.xscale('log')
-    plt.yscale('log')
+    plt.xscale('linear')
+    plt.yscale('linear')
     plt.grid(True)
     plt.show()
 
 if __name__ == "__main__":
     Sample = dict()
-    Sample=reduceADToQR("/home/parallels/Github/Matilda","WAXS.hdf")
+    Sample=reduceADToQR("/home/parallels/Github/Matilda","SAXS.hdf")
+    #Sample["ReducedData"]=test("/home/parallels/Github/Matilda","LaB6_45deg.tif")
     #pp.pprint(Sample)
     PlotResults(Sample)
