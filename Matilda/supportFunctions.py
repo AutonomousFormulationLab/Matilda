4d95b442ff933b17736ee8085522dd81d7433d1f
USAXS account <usaxs@usaxscontrol.xray.aps.anl.gov>
2025-03-23 22:26:37 -0500

    Add function to construct xarray dataset

--- Matilda/supportFunctions.py
+++ Matilda/supportFunctions.py
@@ -3,7 +3,8 @@
 import h5py
 import numpy as np
 import logging
-
+import copy
+import xarray as xr
 ## support stuff here
 # Function to recursively read a group and store its datasets in a dictionary
 def read_group_to_dict(group):
@@ -25,3 +26,34 @@ def read_group_to_dict(group):
             # If the item is a group, recursively read its contents
             data_dict[key] = read_group_to_dict(item)
     return data_dict
+
+def results_to_dataset(results):
+    results = copy.deepcopy(results)
+    ds = xr.Dataset()
+    ds['USAXS_int'] = ('q',results['ReducedData']['UPD'])
+    ds['q'] = results['ReducedData']['Q_array']
+    del results['ReducedData']['UPD']
+    del results['ReducedData']['Q_array']
+    ds.update(results['ReducedData'])
+    for our_name,raw_name in [('AR_angle','ARangles'),
+                              ('TimePerPoint','TimePerPoint'),
+                              ('Monitor','Monitor'),
+                              ('UPD','UPD_array'),
+                             ]:
+        ds[our_name] = ('flyscan_bin',results['RawData'][raw_name])
+        del results['RawData'][raw_name]
+    for our_name,raw_name in [('AmpGain','AmpGain'),
+                              ('AmpReqGain','AmpReqGain'),
+                              ('amp_change_channel','Channel')
+                             ]:
+        ds[our_name] = ('amp_change_channel',results['RawData'][raw_name])
+        del results['RawData'][raw_name]
+                                      
+    ds.attrs.update(results['RawData']['metadata'])
+    del results['RawData']['metadata']
+    ds.attrs['instrument'] = results['RawData']['instrument']
+    del results['RawData']['instrument']
+    ds.update(results['RawData'])
+
+    return ds
+
