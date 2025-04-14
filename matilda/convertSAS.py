70fd4d4015630b6cd0d62f5a6b59504cfa3bd7d2
Jan Ilavsky <jilavsky@gmail.com>
2025-04-14 12:20:40 -0500

    Tested 45 deg tiff file

--- matilda/convertSAS.py
+++ matilda/convertSAS.py
@@ -106,37 +106,39 @@ def ImportAndReduceAD(path, filename):
 
 
 ## test for tilts using LaB6 45 deg tilted detector from GSAXS-II goes here
-def test(path, filename):
-    # read data from tiff file and read the data 
-    my2DData = tiff.imread(path+'/'+filename)
-    wavelength = 0.10798 # in A
-    # pixel_size
-    pixel_size1 = 0.1 # x in Nika, in mm
-    #pixel_size2 = 0.1 # y in Nika, in mm
-    # detector_distance, in mm
-    detector_distance = 1004.91 # in Nika, in mm 
-    # Nika BCX and BCY in pixels
-    BCX = 886.7     # x in Nika
-    BCY = 1048.21   # y in Nika
-    # read Nika HorTilt and VertTilt 
-    HorTilt = 44.7   # x direction in Nika
-    VertTilt = 0.02   # y direction in Nika
-    # poni is geometry file for pyFAI, created by converting first to Fit2D and then calling pyFAI conversion function.
-    my_poni = convert_Nika_to_Fit2D(detector_distance, pixel_size1, BCX, BCY, HorTilt, VertTilt, wavelength)
-    # setup integrator geometry
-    ai = AzimuthalIntegrator(dist=my_poni.dist, poni1=my_poni.poni1, poni2=my_poni.poni2, rot1=my_poni.rot1, rot2=my_poni.rot2,
-                       rot3=my_poni.rot3, pixel1=my_poni.detector.pixel1, pixel2=my_poni.detector.pixel2, 
-                       wavelength=my_poni.wavelength)
-    #create mask here. Duplicate the my2DData and set all values to be masked to NaN, not used here. 
-    mask = np.copy(my2DData)
-    mask = 0*mask           # set all values to zero
-    # Perform azimuthal integration
-    # You can specify the number of bins for the integration
-    #set npt to larger of dimmension of my2DData  `
-    npt = max(my2DData.shape)
-    q, intensity = ai.integrate1d(my2DData, npt, mask=mask, correctSolidAngle=True, unit="q_A^-1")
-    result = {"Intensity":np.ravel(intensity), "Q_array":np.ravel(q)}
-    return result
+# to the best of my undestanding, the images loaded from tiff file are mirrored and the values here are just weird. 
+# def test(path, filename):
+#     # read data from tiff file and read the data 
+#     # tiff files are actually loaded differently than HDF5 files. Looks like they are mirrored. 
+#     my2DData = tiff.imread(path+'/'+filename)
+#     wavelength = 0.10798 # in A
+#     # pixel_size
+#     pixel_size1 = 0.1 # x in Nika, in mm
+#     #pixel_size2 = 0.1 # y in Nika, in mm
+#     # detector_distance, in mm
+#     detector_distance = 1004.91 # in Nika, in mm 
+#     # Nika BCX and BCY in pixels
+#     BCY = 886.7     # this is for hdf5 x in Nika
+#     BCX = 1048.21   # this is for hdf5 y in Nika
+#     # read Nika HorTilt and VertTilt 
+#     VertTilt  = -44.7   # this is negative value for horizontal tilt in Nika
+#     HorTilt = 0.02      # this is value for vertical tilt in Nika, not sure if this shoudl be negative. 
+#     # poni is geometry file for pyFAI, created by converting first to Fit2D and then calling pyFAI conversion function.
+#     my_poni = convert_Nika_to_Fit2D(detector_distance, pixel_size1, BCX, BCY, HorTilt, VertTilt, wavelength)
+#     # setup integrator geometry
+#     ai = AzimuthalIntegrator(dist=my_poni.dist, poni1=my_poni.poni1, poni2=my_poni.poni2, rot1=my_poni.rot1, rot2=my_poni.rot2,
+#                        rot3=my_poni.rot3, pixel1=my_poni.detector.pixel1, pixel2=my_poni.detector.pixel2, 
+#                        wavelength=my_poni.wavelength)
+#     #create mask here. Duplicate the my2DData and set all values to be masked to NaN, not used here. 
+#     mask = np.copy(my2DData)
+#     mask = 0*mask           # set all values to zero
+#     # Perform azimuthal integration
+#     # You can specify the number of bins for the integration
+#     #set npt to larger of dimmension of my2DData  `
+#     npt = max(my2DData.shape)
+#     q, intensity = ai.integrate1d(my2DData, npt, mask=mask, correctSolidAngle=True, unit="q_A^-1")
+#     result = {"Intensity":np.ravel(intensity), "Q_array":np.ravel(q)}
+#     return result
 
 
 def reduceADToQR(path, filename):
@@ -169,7 +171,7 @@ def PlotResults(data_dict):
 
 if __name__ == "__main__":
     Sample = dict()
-    #Sample=reduceADToQR("/home/parallels/Documents/TiltsTest_waxs_fixedMD","LaB6_tilt7v_0049.hdf")
-    Sample["ReducedData"]=test("/home/parallels/Github/Matilda","LaB6_45deg.tif")
+    Sample=reduceADToQR("/home/parallels/Documents/TiltsTest_waxs_fixedMD","LaB6_tilt7v_0049.hdf")
+    #Sample["ReducedData"]=test("/home/parallels/Github/Matilda/TestData","LaB6_45deg.tif")
     #pp.pprint(Sample)
     PlotResults(Sample)
